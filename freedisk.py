ed149be1c875959f4494b501c0fb942a0a146159
zackn9ne <zackn9ne@gmail.com>
2020-04-24 23:06:09 -0400

    Create freedisk.py

---
+++ freedisk.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#gets free diskspace % in python zackn9ne
+
+import subprocess
+cmd = subprocess.Popen(['df', '-h', '/'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+stdout,stderr = cmd.communicate()
+alist = stdout.split()
+#print("the list is") 
+#print(type(alist))
+#print(len(alist))
+print(alist[14])
